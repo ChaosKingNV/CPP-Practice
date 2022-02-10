1955fe2c032b52e7c427846844fb2e86f47cf3da
ChaosKingNV <namannaman.v32@gmail.com>
2022-02-11 00:34:04 +0530

    d

--- rt.cpp
+++ rt.cpp
@@ -1,15 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int main()
-
+/*int makeArray( )
 {
+    
+}*/
 
-int a=10;
-
-cout<<a++;
-
-return 0;
-
+int main() 
+{
+    int numA;
+    int numQ;
+    cin >> numA >> numQ;
+    
+    int size;
+    for (int i = numA; i > 0 ; i--) 
+    {
+        cin >> size;
+        int array[size];
+        for (int j = 0; j < size; j++) 
+        {
+            cin >> array[j];
+        }
+    }
+    return 0;
 }
