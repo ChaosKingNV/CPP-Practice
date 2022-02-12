6424875ca6123224547d1530844b9faf36a3afee
ChaosKingNV <namannaman.v32@gmail.com>
2022-02-13 02:55:40 +0530

    d

--- DSA/rectangle.cpp
+++ DSA/rectangle.cpp
@@ -1,34 +1,76 @@
-#include<iostream>
+#include <iostream>
+
 int main()
 {
-    int n;
-    std::cin >> n;
+    int r;
+    std::cin >> r;
 
+
+    for (int i = 1; i <= (2*r); i++)
+    {
+        if (i <= r )
+        {
+            for (int j = 1; j <= r; j++)
+            {
+                
+                if (j <= (i-1))
+                {
+                    std::cout << "  ";
+                }
+                else
+                {
+                    std::cout << "* ";
+                }
+                
+                
+            }
+            for (int j = (r+1); j <= (2*r); j++)
+            {
+                if (j<=(((2*r)-i)+1))
+                {
+                    std::cout << "* ";
+                }
+                else
+                {
+                    std::cout << "  ";
+                }
+                
+                
+                
+            }
+            
+        }
+        else if (i > r )
+        {
+            for (int j = 1; j <= r; j++)
+            {
+                if (j <= (r-(i-r)))
+                {
+                    std::cout << "  ";
+                }
+                else
+                {
+                    std::cout << "* ";
+                }
+                    
+            }
+            for (int j = (r+1); j <= (2*r); j++)
+            {
+                if (j <= i)
+                {
+                    std::cout << "* ";
+                }
+                else
+                {
+                    std::cout << "  ";
+                }
+                
+                
+            }
+                
+        }
+       
+        std::cout << std::endl;
+    }
     
-
-    for (int i= 1; i<=n; i++)
-    {
-        for(int j=1; j<=n-i; j++)
-        {
-            std::cout << " ";
-        }
-        for(int j=1; j<=2*i-1; j++)
-        {
-            std::cout << "*";
-        }
-        std::cout << std::endl;
-    }
-    for (int i=n; i>0; i--)
-    {
-        for(int j=1; j<=n-i; j++)
-        {
-            std::cout << " ";
-        }
-        for(int j=1; j<=2*i-1; j++)
-        {
-            std::cout << "*";
-        }
-        std::cout << std::endl;
-    }
-    return 0;
 }
