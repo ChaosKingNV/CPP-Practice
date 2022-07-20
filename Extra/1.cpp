a3eea340ec358e3934cdc5bfa21377f5521507c6
ChaosKingNV <namannaman.v32@gmail.com>
2022-07-20 19:57:05 +0530

    Deleted all.exe files

--- Extra/1.cpp
+++ Extra/1.cpp
@@ -1,29 +1,10 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 
-class Student {
-public: 
-    int roll; 
-    string Name; 
-    float percent; 
-};
 
 int main()
 {
-    
-    Student std;
-
-    
-    std.roll = 101;
-    std.Name = "Naman Vashishta";
-    std.percent = 94.32f;
-
-    
-    cout << "Student's Roll No.: " << std.roll << "\n";
-    cout << "Student's Name: " << std.Name << "\n";
-    cout << "Student's Percentage: " << std.percent << "\n";
-
+    std::cout << "hello world";
     return 0;
 }
